39778c14bc696aad7f175188946b61e76b0a60ad
mateus <ffrmateus@gmail.com>
2021-12-08 16:57:48 -0300

    Efetuando Login na Administração

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var users = require('./../inc/users')
 var router = express.Router();
 
 router.get("/", function(req,res,next){
@@ -8,14 +9,39 @@ router.get("/", function(req,res,next){
 
 });
 
+router.post("/login", function(req,res,next){
+
+    if(!req.body.email){
+        
+        users.render(req,res, "Preencha o campo e-mail");
+
+    }else if(!req.body.password){
+
+        users.render(req,res, "Preencha o campo senha");
+
+    }else{
+
+        users.login(req.body.email,req.body.password).then(user =>{
+
+
+            req.session.user = user;
+            
+            res.redirect("/admin");
+
+        }).catch(err =>{
+
+            users.render(req,res,err.message || err);
+
+        })
+
+    }
+
+})
+
 router.get("/login", function(req,res,next){
 
-    console.log(req.session)
 
-   if(!req.session.views) req.session.views =0;
-
-        console.log("SESSION",req.session.views++)
-    res.render("admin/login");
+    users.render(req,res,null);
 
 
 });
