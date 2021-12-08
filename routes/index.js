b6c628f1cb05c8caeff942fe6242b367f0c89702
mateus <ffrmateus@gmail.com>
2021-12-08 13:25:23 -0300

    Organizando o Conteúdo com Includes

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,20 @@
 var conn = require ('./../inc/db')
 var express = require('express');
+var menus = require('./../inc/menus')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-  `, (err, results)=>{
+  menus.getMenus().then(results =>{
 
-    if(err){
-
-      console.log(err);
-
-    }
-    
     res.render('index', { 
       title: 'Restaurante Saboroso!',
       menus: results 
     });
   
   });
+
 
 });
 
@@ -36,11 +30,18 @@ router.get('/contacts', function(req,res,next){
 
 router.get('/menus', function(req,res,next){
 
-  res.render('menus',{
+  menus.getMenus().then(results =>{
+
+    res.render('menus',{
       title: 'Menus - Restaurante Saboroso!',
       background: 'images/img_bg_1.jpg',
-      h1:'Saboreie nosso menu!'
+      h1:'Saboreie nosso menu!',
+      menus: results
     });
+
+  })
+
+  
 
 });
 
