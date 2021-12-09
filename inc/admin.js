a97a0dd96da9c6f4817aff890f7977a414e548b8
mateus <ffrmateus@gmail.com>
2021-12-09 13:56:09 -0300

    Exibindo dados do usuário autenticado

--- inc/admin.js
+++ inc/admin.js
@@ -1,4 +1,16 @@
+
 module.exports ={
+
+    getParams(req, params){
+
+        return Object.assign({},{
+
+            menus:req.menus,
+            user: req.session.user
+
+        }, params)
+
+    },
 
     getMenus(req){
 
